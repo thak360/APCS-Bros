39e9b62186f3e5a12bae546cc154acfe5efb1257
jtpwolf <jtpeters16@gmail.com>
2015-05-20 09:15:27 -0700

    Update World.java

--- src/com/rhughes/bros/world/World.java
+++ src/com/rhughes/bros/world/World.java
@@ -23,6 +23,11 @@ public class World {
 	private int[] pixels;
 	
 	public World(String path) {
+		imageRead(path);
+	}
+	
+	public void imageRead(String path)
+	{
 		BufferedImage image = null;
 		try {
 			image = ImageIO.read(new File(Reference.SPRITE_LOCATION + path));
@@ -32,19 +37,12 @@ public class World {
 		width = image.getWidth();
 		height = image.getHeight();
 		pixels = image.getRGB(0, 0, width, height, pixels, 0, width);
-		for(int y = 0; y < height; y++) {
-			for(int x = 0; x < width; x++) {
-				if(BlockLib.getFromId(pixels[x + y *width], x, y) != null) {
-					blocks.add(BlockLib.getFromId(pixels[x + y * width], x, y));
-					//System.out.println("block");
-				}
-			}
-		}
-		for(int i = 500; i > 500 - 128; i -= 64) {
-			for(int j = 0; j < 25; j++) {
-				if(BlockLib.getFromId(10, j*32, 250) != null) {
-					blocks.add(BlockLib.getFromId(10, j*32 + i, i));
-				}
+		for(int i = 0; i< width; i ++){
+			for(int j =0; j<height;j++){
+				if(pixels[i+j*width]==pixels[359])
+					blocks.add(new Block(blockMap.get(1), i*32,j*32));
+				if(pixels[i+j*width]==pixels[440])
+					blocks.add(new Block(blockMap.get(3), i*32,j*32));
 			}
 		}
 	}
