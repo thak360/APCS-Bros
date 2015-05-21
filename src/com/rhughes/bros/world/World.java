ad729a922230c7976f3bed2bf1d0f3352b1a5524
jtpwolf <jtpeters16@gmail.com>
2015-05-21 16:31:05 -0700

    Update World.java

--- src/com/rhughes/bros/world/World.java
+++ src/com/rhughes/bros/world/World.java
@@ -5,6 +5,7 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Map;
 
 import javax.imageio.ImageIO;
@@ -12,6 +13,7 @@ import javax.imageio.ImageIO;
 import com.rhughes.bros.entities.Entity;
 import com.rhughes.bros.entities.Player;
 import com.rhughes.bros.gfx.Sprite;
+import com.rhughes.bros.gfx.SpriteSheet;
 import com.rhughes.bros.libs.BlockLib;
 import com.rhughes.bros.libs.Reference;
 
@@ -27,6 +29,11 @@ public class World {
 	int color = 2;
 	
 	public World(String path) {
+		imageRead(path);
+	}
+	
+	public void imageRead(String path)
+	{
 		BufferedImage image = null;
 		try {
 			image = ImageIO.read(new File(Reference.SPRITE_LOCATION + path));
@@ -36,29 +43,27 @@ public class World {
 		width = image.getWidth();
 		height = image.getHeight();
 		pixels = image.getRGB(0, 0, width, height, pixels, 0, width);
-		for(int i = 0; i < 4000; i+=32) {
-				if(blockMap.get(color) != null) {      
-					blocks.add(new Block(blockMap.get(color),i+500,500));
-				}
-			}
-		for(int j = 100; j>=0; j-=32)
-		{
-			if(blockMap.get(color) != null) {     
-				blocks.add(new Block(blockMap.get(color),500,500-j));
-			}
-		}
-		for(int j = 100; j>=0; j-=32)
-		{
-			if(blockMap.get(color) != null) {      
-				blocks.add(new Block(blockMap.get(color),4500,500-j));
+		for(int i = 0; i< width; i ++){
+			for(int j =0; j<height;j++){
+				if(pixels[i+j*width]==pixels[9*70])
+					blocks.add(new Block(blockMap.get(1), i*32,j*32));
+				if(pixels[i+j*width]==pixels[14*70])
+					blocks.add(new Block(blockMap.get(3),i*32, j*32));
+				if(pixels[i+j*width]==pixels[69+8*70])
+					blocks.add(new Block(blockMap.get(2), i*32,j*32));
 			}
 		}
-		for(int i = 0; i<=100; i+=32)
-		{
-			if(blockMap.get(color) != null) {      
-				blocks.add(new Block(blockMap.get(color),1200+i,416));
-			}
+	}
+	
+	public Block find(int x, int y){
+		for (int i = 0; i < blocks.size(); i++){
+			int x2 = blocks.get(i).getX();
+			int y2 = blocks.get(i).getY();
+			if (x > x2 && x < x2 + 32)
+				if(y > y2 && y < y2 + 32)
+					return blocks.get(i);
 		}
+		return null;
 	}
 	
 	public void render(Graphics g) {
