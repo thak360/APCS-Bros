d7063a62cf6c3969855b31b1c449044cb5f2b6de
jtpwolf <jtpeters16@gmail.com>
2015-05-22 10:00:33 -0700

    Update Images.java

--- src/com/rhughes/bros/libs/Images.java
+++ src/com/rhughes/bros/libs/Images.java
@@ -5,6 +5,7 @@ import java.awt.image.BufferedImage;
 public class Images {
 	
 	public static BufferedImage title;
+	public static BufferedImage gameOver;
 	
 	public static BufferedImage spritesheet;
 	
