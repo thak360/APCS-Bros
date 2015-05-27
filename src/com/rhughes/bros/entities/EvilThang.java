5ea42f9571f1aa613b204e6c45c2bf5e5b7b9902
jtpwolf <jtpeters16@gmail.com>
2015-05-27 13:21:58 -0700

    Update EvilThang.java

--- src/com/rhughes/bros/entities/EvilThang.java
+++ src/com/rhughes/bros/entities/EvilThang.java
@@ -1,20 +1,24 @@
 package com.rhughes.bros.entities;
 
 import java.awt.Rectangle;
+import java.awt.event.KeyEvent;
 
 import com.rhughes.bros.enums.Direction;
 import com.rhughes.bros.gfx.Animation;
 import com.rhughes.bros.gfx.Sprite;
 import com.rhughes.bros.gfx.SpriteSheet;
+import com.rhughes.bros.input.KeyInput;
 import com.rhughes.bros.world.Block;
 import com.rhughes.bros.world.World;
 
 public class EvilThang extends Mob{
 	
-	private static SpriteSheet sheet = new SpriteSheet("Mex.png");
+	private static SpriteSheet sheet = new SpriteSheet("Enemy.png");
 
 	public EvilThang(int x, int y, World world) {
 		super(x, y, world);
+		sprite = new Sprite(2, 1, 50, sheet);
+		sprite2 = new Sprite(1, 1, 50, sheet);
 		Sprite[] rights = new Sprite[]
 		        {
 		                new Sprite(1, 1, 46, sheet),
@@ -31,30 +35,34 @@ public class EvilThang extends Mob{
 		                new Sprite(4, 2, 46, sheet)
 		                
 		        };
-		animeLeft = new Animation(4, lefts);
-		animeRight = new Animation(4, rights);
+		animeRight = new Animation(5, rights);
+		animeLeft = new Animation(5, lefts);
 	}
 
 	@Override
 	public boolean hasHorizontalCollision() {
 		for(int i = 0; i < world.getBlocks().size(); i ++) {
 			Block block = world.getBlocks().get(i);
-			if(getRight().intersects(block.getBounds())) return true;
-			if(getLeft().intersects(block.getBounds())) return true;
+			if(getRight().intersects(block.getRectangle())) return true;
+			if(getLeft().intersects(block.getRectangle())) return true;
 		}
 		return false;
 	}
 
+	public Rectangle getRectangle() {
+        return new Rectangle(x, y, 32, 50);
+    }
+	
 	@Override
 	public boolean hasVerticalCollision() {
 		for(int i = 0; i < world.getBlocks().size(); i ++) {
 			Block block = world.getBlocks().get(i);
-			if(getBottom().intersects(block.getBounds()) && dy > 0){
+			if(getBottom().intersects(block.getRectangle()) && dy > 0){
 				falling = false;
 				jumping = false;
 				return true;
 			}
-			if(getTop().intersects(block.getBounds()) && dy < 0) {
+			if(getTop().intersects(block.getRectangle()) && dy < 0) {
 				dy = 0;
 				return true;
 			}
@@ -65,22 +73,21 @@ public class EvilThang extends Mob{
 	public void tick() {
 		dx = 0;
 		if (super.getDirection() == Direction.Right)
-			dx += 15;
+			dx += 2;
 		else
-			dx -= 15;
+			dx -= 2;
 		if(direction == Direction.Left)
-			if (world.find(x - 1, y + 47).equals(null))
+			if (world.find(x - 1, y + 47).equals(world.blocks.get(0)))
 				dx *= -1;
 			else if(world.find(x + 47, y + 47).equals(null))
 				dx *= -1;
-		else if (hasHorizontalCollision())
+		if (hasHorizontalCollision())
 			dx *= -1;
 		if (dx > 0)
 			direction = Direction.Right;
 		if (dx < 0)
 			direction = Direction.Left;
-		if(!hasHorizontalCollision())
-			x += dx;
+		x += dx;
 	}
 
 	@Override
@@ -101,10 +108,6 @@ public class EvilThang extends Mob{
 	@Override
 	public Rectangle getRight() {
 		return new Rectangle(x + 5, y + 3, 4, 43);
-	}
-	
-	public void die() {
-		world.removeEntity(this);
 	}
 
 }
