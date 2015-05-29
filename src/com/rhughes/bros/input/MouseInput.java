da83fb821e86b2cba4042c85679b2ec340702f12
jtpwolf <jtpeters16@gmail.com>
2015-05-29 13:04:20 -0700

    Update MouseInput.java

--- src/com/rhughes/bros/input/MouseInput.java
+++ src/com/rhughes/bros/input/MouseInput.java
@@ -34,12 +34,14 @@ public class MouseInput extends MouseAdapter {
         int mouse = e.getButton(); //used to check what button was clicked
         clickX = e.getX();
         clickY = e.getY();
-        if(Game.menu.play.contains(clickX, clickY)||Game.pause.resume.contains(clickX, clickY))
+        if((Game.menu.play.contains(clickX, clickY)&&Game.state==GameState.Menu)||((Game.pause.resume.contains(clickX, clickY)&&Game.state==GameState.Pause)))
         	Game.state = GameState.Game;
-        if(Game.gameOver.restart.contains(clickX, clickY)/*||Game.pause.restart.contains(clickX,clickY)*/)
+        else if((Game.gameOver.restart.contains(clickX, clickY)&&Game.state==GameState.GameOver)||((Game.pause.restart.contains(clickX, clickY)&&Game.state==GameState.Pause)))
         	Game.state = GameState.Menu;
-        if(Game.gameOver.quit.contains(clickX, clickY)||Game.menu.quit.contains(clickX, clickY)/*||Game.pause.quit.contains(clickX, clickY)*/)
+        else if((Game.gameOver.quit.contains(clickX, clickY)&&Game.state==GameState.GameOver)||(Game.menu.quit.contains(clickX, clickY)&&Game.state==GameState.Menu)||(Game.pause.quit.contains(clickX, clickY)&&Game.state==GameState.Pause))
         	Game.exit();
+        else
+        	return;
     }
 
     @Override
