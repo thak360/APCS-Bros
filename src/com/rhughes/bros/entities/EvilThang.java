7c28debe86249424271cfb52ba72f7193fe9edc9
jtpwolf <jtpeters16@gmail.com>
2015-05-29 13:16:04 -0700

    Update EvilThang.java

--- src/com/rhughes/bros/entities/EvilThang.java
+++ src/com/rhughes/bros/entities/EvilThang.java
@@ -41,12 +41,18 @@ public class EvilThang extends Mob{
 
 	@Override
 	public boolean hasHorizontalCollision() {
-		for(int i = 0; i < world.getBlocks().size(); i ++) {
-			Block block = world.getBlocks().get(i);
-			if(getRight().intersects(block.getRectangle())) return true;
-			if(getLeft().intersects(block.getRectangle())) return true;
-		}
-		return false;
+			for(int i = 0; i < world.getBlocks().size(); i ++) {
+				Block block = world.getBlocks().get(i);
+				if(getLeft().intersects(block.getRectangle())){
+					setPosition(x + 1, y);
+					return true;
+				}
+				if(getRight().intersects(block.getRectangle())){
+					setPosition(x - 1, y);
+					return true;
+				}
+			}
+			return false;
 	}
 
 	public Rectangle getRectangle() {
@@ -55,59 +61,56 @@ public class EvilThang extends Mob{
 	
 	@Override
 	public boolean hasVerticalCollision() {
-		for(int i = 0; i < world.getBlocks().size(); i ++) {
+		for(int i = 0; i < world.getBlocks().size(); i++) {
 			Block block = world.getBlocks().get(i);
-			if(getBottom().intersects(block.getRectangle()) && dy > 0){
-				falling = false;
-				jumping = false;
+			if(getTop().intersects(block.getRectangle())){
+				setPosition(x, y + 1);
+				dy=0;
 				return true;
 			}
-			if(getTop().intersects(block.getRectangle()) && dy < 0) {
-				dy = 0;
-				return true;
+			if(getBottom().intersects(block.getRectangle())){
+				setPosition(x, y - 1);
+				dy=0;
+				falling=false;
 			}
 		}
 		return false;
 	}
 	
 	public void tick() {
-		dx = 0;
-		if (super.getDirection() == Direction.Right)
-			dx += 2;
-		else
-			dx -= 2;
+		if(direction == Direction.Right)
+			dx = 2;
 		if(direction == Direction.Left)
-			if (world.find(x - 1, y + 47).equals(world.blocks.get(0)))
-				dx *= -1;
-			else if(world.find(x + 47, y + 47).equals(null))
-				dx *= -1;
-		if (hasHorizontalCollision())
-			dx *= -1;
-		if (dx > 0)
-			direction = Direction.Right;
-		if (dx < 0)
-			direction = Direction.Left;
-		x += dx;
+			dx = -2;
+		if(hasHorizontalCollision()){
+			if(direction == Direction.Right){
+				direction = Direction.Left;
+			}
+			else{
+				direction=Direction.Right;
+			}
+		}
+		super.tick();
 	}
 
 	@Override
-	public Rectangle getTop() {
-		return new Rectangle(x + 15, y + 3, 12, 4);
-	}
+    public Rectangle getTop() {
+        return new Rectangle(x + 16, y + 4, 12, 4);
+    }
 
-	@Override
-	public Rectangle getBottom() {
-		return new Rectangle(x + 19, y + 42, 10, 4);
-	}
+    @Override
+    public Rectangle getBottom() {
+        return new Rectangle(x + 16, y + 46, 12, 4);
+    }
 
-	@Override
-	public Rectangle getLeft() {
-		return new Rectangle(x + 34, y + 3, 4, 43);
-	}
+    @Override
+    public Rectangle getRight() {
+        return new Rectangle(x + 37, y + 4, 4, 40);
+    }
 
-	@Override
-	public Rectangle getRight() {
-		return new Rectangle(x + 5, y + 3, 4, 43);
-	}
+    @Override
+    public Rectangle getLeft() {
+        return new Rectangle(x + 10, y + 8, 4, 40);
+    }
 
 }
