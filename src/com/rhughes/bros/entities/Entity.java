09918329ee6cbaf25c9defd73856ec2d507a3e92
jtpwolf <jtpeters16@gmail.com>
2015-05-29 13:17:03 -0700

    Update Entity.java

--- src/com/rhughes/bros/entities/Entity.java
+++ src/com/rhughes/bros/entities/Entity.java
@@ -1,43 +1,125 @@
-package com.rhughes.bros.entities;
+package com.rhughes.bros.world;
 
 import java.awt.Graphics;
-import java.awt.Rectangle;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
+import javax.imageio.ImageIO;
+
+import com.rhughes.bros.entities.Entity;
+import com.rhughes.bros.entities.Player;
 import com.rhughes.bros.gfx.Sprite;
-import com.rhughes.bros.world.World;
+import com.rhughes.bros.gfx.SpriteSheet;
+import com.rhughes.bros.libs.BlockLib;
+import com.rhughes.bros.libs.Reference;
 
-// Made to be a skeleton of any in game object
-// if it is a mob, sprite must be a sprite of the mob facing left standing still
+public class World {
+	
+	ArrayList<Entity> entities = new ArrayList<Entity>();
+	public ArrayList<Block> blocks = new ArrayList<Block>();
+	
+	private int width;
+	private int height;
+	private static int[] pixels;
+	int color = 2;
+	private int finishBlock=0;
+	private boolean doneCount=false;
+	
+	public World(String path) {
+		imageRead(path);
+	}
+	
+	public ArrayList<Block> getBlockArray()
+	{
+		return blocks;
+	}
+	
+	public void imageRead(String path)
+	{
+		BufferedImage image = null;
+		try {
+			image = ImageIO.read(new File(Reference.SPRITE_LOCATION + path));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		width = image.getWidth();
+		height = image.getHeight();
+		pixels = image.getRGB(0, 0, width, height, pixels, 0, width);
+		for(int i = 0; i< width; i ++){
+			for(int j =0; j<height;j++){
+				if(pixels[i+j*width]==pixels[9*70])
+				{
+					blocks.add(new Block(BlockLib.blockMap.get(1), i*32,j*32));
+					if(!doneCount)finishBlock++;
+				}
+				if(pixels[i+j*width]==pixels[14*70])
+				{
+					blocks.add(new Block(BlockLib.blockMap.get(3),i * 32, j * 32));
+					if(!doneCount)finishBlock++;
+				}
+				if(pixels[i+j*width]==pixels[69+8*70])
+				{
+					blocks.add(new Block(BlockLib.blockMap.get(2), i * 32,j*32));
+					doneCount=true;
+				}
+			}
+		}
+	}
+	
+	
+	public int getFinishBlock()
+	{
+		return finishBlock;
+	}
+	
+	public Block find(int x, int y){
+		for (Block block : blocks){
+			int x2 = block.getX();
+			int y2 = block.getY();
+			if (x > x2 && x < x2 + 32)
+				if(y > y2 && y < y2 + 32)
+					return block;
+		}
+		return blocks.get(0);
+	}
+	
+	public void render(Graphics g) {
+		for(Block block : blocks)
+			block.render(g);
+		for(Entity ent : entities)
+			ent.render(g);
+	}
+	
+	public void tick() {
+		for(Entity ent : entities)
+			ent.tick();
+	}
+	
+	public void addEntity (Entity ent) {
+		entities.add(ent);
+	}
+	
+	public ArrayList<Entity> getEntities() {
+		return entities;
+	}
+	
+	public ArrayList<Block> getBlocks() {
+		return blocks;
+	}
+	
+	public void removeEntity(int index) {
+		entities.remove(index);
+	}
+	
+	public Player getPlayer() {
+		for (Entity ent : entities)
+			if(ent instanceof Player)
+				return (Player)ent;
+		return null;
+	}
 
-public abstract class Entity {
-	
-	protected int x, y;
-	protected World world;
-	protected Sprite sprite;
-	
-	public Entity(int x, int y, World world) {
-		this.x = x;
-		this.y = y;
-		this.world = world;
-		world.addEntity(this);
-	}
-	
-	// methods to get the bounds of the entity
-	public abstract Rectangle getTop();
-	public abstract Rectangle getBottom();
-	public abstract Rectangle getLeft();
-	public abstract Rectangle getRight();
-	
-	public void setPosition(int x, int y) {
-		this.x = x;
-		this.y = y;
-	}
-	
-	// render method necessary to print to screen for each class
-	public  void render(Graphics g) {
-		sprite.render(g, x, y);
-	}
-	
-	// the method to make the entity "do" whatever it is coded to do
-	public abstract void tick();
 }
