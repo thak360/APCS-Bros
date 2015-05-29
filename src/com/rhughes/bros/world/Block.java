ec0398448426dcf01f1528092ed6319d617a6abb
jtpwolf <jtpeters16@gmail.com>
2015-05-29 12:58:22 -0700

    Update Block.java

--- src/com/rhughes/bros/world/Block.java
+++ src/com/rhughes/bros/world/Block.java
@@ -16,6 +16,12 @@ public class Block {
 		this.y = y;
 	}
 	
+	public int getX()
+	{return x;}
+	
+	public int getY()
+	{return y;}
+	
 	public void render(Graphics g) {
 		sprite.render(g, x, y);
 	}
@@ -24,27 +30,45 @@ public class Block {
         return new Rectangle(x, y, 32, 32);
     }
 
-    public Rectangle getTop() {
-        return new Rectangle(x + 4, y, 24, 4);
-    }
-
-    public Rectangle getBottom() {
-        return new Rectangle(x + 4, y + 28, 24, 4);
-    }
-
-    public Rectangle getRight() {
-        return new Rectangle(x, y, 4, 32);
-    }
-
-    public Rectangle getLeft() {
-        return new Rectangle(x + 28, y, 4, 32);
-    }
-    
     public Rectangle getRectangle() {
-    	return new Rectangle(x, y, 32, 32);
+        return new Rectangle(x, y, 32, 32);
     }
-    
-    public int getX(){return x;}
-    public int getY(){return y;}
+
+}
+package com.rhughes.bros.world;
+
+import java.awt.Graphics;
+import java.awt.Rectangle;
+
+import com.rhughes.bros.gfx.Sprite;
+
+public class Block {
+	
+	private int x, y;
+	private Sprite sprite;
+
+	public Block(Sprite sprite, int x, int y) {
+		this.sprite = sprite;
+		this.x = x;
+		this.y = y;
+	}
+	
+	public int getX()
+	{return x;}
+	
+	public int getY()
+	{return y;}
+	
+	public void render(Graphics g) {
+		sprite.render(g, x, y);
+	}
+	
+	public Rectangle getBounds() {
+        return new Rectangle(x, y, 32, 32);
+    }
+
+    public Rectangle getRectangle() {
+        return new Rectangle(x, y, 32, 32);
+    }
 
 }
