310335c2bd326d7e62bc6868ba1907ee83de67d1
jtpwolf <jtpeters16@gmail.com>
2015-05-29 12:25:10 -0700

    Update World.java

--- src/com/rhughes/bros/world/World.java
+++ src/com/rhughes/bros/world/World.java
@@ -5,29 +5,42 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.imageio.ImageIO;
 
 import com.rhughes.bros.entities.Entity;
 import com.rhughes.bros.entities.Player;
+import com.rhughes.bros.gfx.Sprite;
+import com.rhughes.bros.gfx.SpriteSheet;
 import com.rhughes.bros.libs.BlockLib;
 import com.rhughes.bros.libs.Reference;
 
 public class World {
 	
-	private ArrayList<Entity> entities = new ArrayList<Entity>();
-	private ArrayList<Block> blocks = new ArrayList<Block>();
+	ArrayList<Entity> entities = new ArrayList<Entity>();
+	public ArrayList<Block> blocks = new ArrayList<Block>();
 	
 	private int width;
 	private int height;
-	private int[] pixels;
+	private static int[] pixels;
+	private Map<Integer, Sprite> blockMap=BlockLib.getBlockMap();
+	int color = 2;
+	private int finishBlock=0;
+	private boolean doneCount=false;
+	
+	public World(String path) {
+		imageRead(path);
+	}
 	
 	public ArrayList<Block> getBlockArray()
 	{
 		return blocks;
 	}
 	
-	public World(String path) {
+	public void imageRead(String path)
+	{
 		BufferedImage image = null;
 		try {
 			image = ImageIO.read(new File(Reference.SPRITE_LOCATION + path));
@@ -42,25 +55,37 @@ public class World {
 				if(pixels[i+j*width]==pixels[9*70])
 				{
 					blocks.add(new Block(blockMap.get(1), i*32,j*32));
-					if(!doneCount) finishBlock++;
+					if(!doneCount)finishBlock++;
 				}
 				if(pixels[i+j*width]==pixels[14*70])
 				{
 					blocks.add(new Block(blockMap.get(3),i*32, j*32));
-					if(!doneCount) finishBlock++;
+					if(!doneCount)finishBlock++;
 				}
-				if(pixels[i+j*width] == pixels[69+8*70])
+				if(pixels[i+j*width]==pixels[69+8*70])
 				{
 					blocks.add(new Block(blockMap.get(2), i*32,j*32));
-					doneCount = true;
+					doneCount=true;
 				}
 			}
 		}
 	}
 	
+	
 	public int getFinishBlock()
 	{
 		return finishBlock;
+	}
+	
+	public Block find(int x, int y){
+		for (Block block : blocks){
+			int x2 = block.getX();
+			int y2 = block.getY();
+			if (x > x2 && x < x2 + 32)
+				if(y > y2 && y < y2 + 32)
+					return block;
+		}
+		return blocks.get(0);
 	}
 	
 	public void render(Graphics g) {
@@ -87,8 +112,8 @@ public class World {
 		return blocks;
 	}
 	
-	public void removeEntity(Entity ent) {
-		entities.remove(ent);
+	public void removeEntity(int index) {
+		entities.remove(index);
 	}
 	
 	public Player getPlayer() {
@@ -96,17 +121,6 @@ public class World {
 			if(ent instanceof Player)
 				return (Player)ent;
 		return null;
-	}
-	
-	public Block find(int x, int y){
-		for (Block block : blocks){
-			int x2 = block.getX();
-			int y2 = block.getY();
-			if (x > x2 && x < x2 + 32)
-				if(y > y2 && y < y2 + 32)
-					return block;
-		}
-		return blocks.get(0);
 	}
 
 }
