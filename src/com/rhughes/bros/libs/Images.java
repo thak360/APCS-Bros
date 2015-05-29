fb66187fba675014ca2422cf23925c0693629d0a
jtpwolf <jtpeters16@gmail.com>
2015-05-29 13:02:33 -0700

    Update Images.java

--- src/com/rhughes/bros/libs/Images.java
+++ src/com/rhughes/bros/libs/Images.java
@@ -5,12 +5,12 @@ import java.awt.image.BufferedImage;
 public class Images {
 	
 	public static BufferedImage title;
+	public static BufferedImage gameOver;
+	public static BufferedImage enemy;
 	
 	public static BufferedImage spritesheet;
 	
 	public static BufferedImage blockGreen;
 	public static BufferedImage blockBlue;
-	
-	public static BufferedImage gameOver;
 
 }
