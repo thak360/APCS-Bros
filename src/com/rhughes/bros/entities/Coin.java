2cd17d451bcaf3e726a7b3d1e0d07acadec1015d
Finn <finnbiggs@gmail.com>
2015-05-31 17:45:23 -0700

    Update Coin.java

--- src/com/rhughes/bros/entities/Coin.java
+++ src/com/rhughes/bros/entities/Coin.java
@@ -3,6 +3,7 @@ package com.rhughes.bros.entities;
 import java.awt.Graphics;
 import java.awt.Rectangle;
 
+import com.rhughes.bros.Game;
 import com.rhughes.bros.gfx.Animation;
 import com.rhughes.bros.gfx.Sprite;
 import com.rhughes.bros.world.World;
@@ -13,7 +14,7 @@ public class Coin extends Entity
 {
 	private static SpriteSheet sheet = new SpriteSheet("coin.png");
 	private Animation anime;
-	
+	public boolean paidOut = false;
 	public Coin(int x, int y, World world)
 	{
 		super(x, y, world);
@@ -35,28 +36,24 @@ public class Coin extends Entity
 		{}
 	
 	public void render(Graphics g)
-	{
-		anime.runAnimation(g, x, y);
-	}
+		{anime.runAnimation(g, x, y);}
 	
 	public  Rectangle getTop()
-	{
-		return new Rectangle(x + 6, y, 18, 4);
-	}
+		{return new Rectangle(x + 6, y, 18, 4);}
 	
 	public Rectangle getBottom()
-	{
-		return new Rectangle(x + 6, y + 28, 18, 4);
-	}
+		{return new Rectangle(x + 6, y + 28, 18, 4);}
 	
 	public Rectangle getLeft()
-	{
-		return new Rectangle(x, y + 6, 4, 20);
-	}
+		{return new Rectangle(x, y + 6, 4, 20);}
 	
 	public Rectangle getRight()
-	{
-		return new Rectangle(x + 26, y + 6, 20, 4);
-	}
+		{return new Rectangle(x + 26, y + 6, 20, 4);}
 	
+	public void die()
+	{
+		Game.world.deathBin.add(this);
+		paidOut = true;
+	}
+
 }
