d94965658dd2d2a5df700b760aecd8eeb3ed475f
Finn <finnbiggs@gmail.com>
2015-06-03 23:32:13 -0700

    Update Hud.java

--- src/com/rhughes/bros/screens/Hud.java
+++ src/com/rhughes/bros/screens/Hud.java
@@ -6,7 +6,7 @@ import java.awt.Graphics;
 import com.rhughes.bros.Game;
 
 public class Hud {
-	
+	//Simply displays the players score, calculated as 100 * number of coins collected
 	public static void render(Graphics g) {
 		g.setColor(Color.yellow);
 		g.drawString("Score: " + (Game.player.score*100), 50, 50);
